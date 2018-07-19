49680882cbbcb7f54812cb472421abd6700a5870
alejo_leon <alejo_leon@hotmail.es>
2018-07-18 19:38:47 -0500

    Add: Books file and collection component update.

--- src/app/main-item-collections/main-item-collections.component.ts
+++ src/app/main-item-collections/main-item-collections.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { books } from '../data-books';
 
 @Component({
   selector: 'main-item-collections',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainItemCollectionsComponent implements OnInit {
 
-  constructor() { }
+  booksList: any[];
+  constructor() { 
+    this.booksList = [];
+  }
 
   ngOnInit() {
+    this.booksList = books.items;
   }
 
 }
