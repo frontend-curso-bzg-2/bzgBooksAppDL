73d3feb3e807f1c17e5060e50cbc6060b7022e6f
Juan Duarte <jukdulinux@gmail.com>
2018-07-26 17:50:45 -0500

    Adding module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, BrowserAnimationsModule,
     RouterModule.forRoot(routes),
     CoreModule
   ],
