90d00aaee773765bd536ba8158d384adc266f241
alejo_leon <alejo_leon@hotmail.es>
2018-08-02 19:16:12 -0500

    Production Firebase keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCIyIeSNwmppevuE3_IDTSvT0KTe89R1g8',
+    authDomain: 'bzgbooksappdl.firebaseapp.com',
+    databaseURL: 'https://bzgbooksappdl.firebaseio.com/',
+    projectId: 'bzgbooksappdl'
+  }
 };
