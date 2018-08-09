f26d3ef9c29872c185ecb4ad06d081e2f4c21c9d
alejo_leon <alejo_leon@hotmail.es>
2018-08-09 18:07:21 -0500

    Add Modules: AngularFireDatabaseModule, StoreModule, StoreDevtoolsModule (ngrx)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from "angularfire2";
 import { AngularFireAuthModule } from "angularfire2/auth";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+
+import {StoreModule} from {"@ngrx/store"};
+import {StoreDevtoolsModule} from {"@ngrx/store-devtools"};
+
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { BooksDBService } from "./api-data/api-books-demo";
@@ -26,7 +31,10 @@ import { environment } from "../environments/environment";
     HttpModule,
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    StoreModule,
+    StoreDevtoolsModule
     /*HttpClientInMemoryWebApiModule.forRoot(
       BooksDBService, { dataEncapsulation: false, delay: 1500 }
     )*/
