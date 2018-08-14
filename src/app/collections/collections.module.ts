b7b681f84b065de5e511af1d332288d593559483
Juan Duarte <juan.duarte@bizagi.com>
2018-08-14 17:17:04 -0500

    Adding components for collection management.

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -4,13 +4,14 @@ import { RouterModule } from "@angular/router";
 
 import { routes } from "./routers.collections";
 import { MainItemCollectionsComponent } from './containers/main-item-collections/main-item-collections.component';
+import { MenuListComponent } from './components/menu-list/menu-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [MainItemCollectionsComponent],
+  declarations: [MainItemCollectionsComponent, MenuListComponent],
   exports: [MainItemCollectionsComponent]
 })
 export class CollectionsModule { }
---
+++ src/app/collections/components/menu-list/menu-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'menu-list',
+  templateUrl: './menu-list.component.html',
+  styleUrls: ['./menu-list.component.styl']
+})
+export class MenuListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
