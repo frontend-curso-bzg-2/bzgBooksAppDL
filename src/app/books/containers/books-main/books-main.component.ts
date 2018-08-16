a73b83baa71b76e0cc88ee4761e9d4ed8a82fc41
alejo_leon <alejo_leon@hotmail.es>
2018-08-15 19:41:24 -0500

    Merge with drag and drop component and styles fix.

--- src/app/books/containers/books-main/books-main.component.ts
+++ src/app/books/containers/books-main/books-main.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { BooksListService } from "../../services/list/books-list.service";
 import { BookList } from '../../models/books';
-
+import { Store, select } from '@ngrx/store';
+import * as fromAuth from "../../../authentication/reducers/";
+import { Observable } from 'rxjs';
+import { Collection } from '../../../collections/models/collections';
+import { CollectionService } from '../../../collections/services/collection.service';
 @Component({
   selector: 'books-main',
   templateUrl: './books-main.component.html',
   styleUrls: ['./books-main.component.styl']
 })
 export class BooksMainComponent implements OnInit {
-
+  collectionList: Observable<Collection[]>;
+  user$:Observable<string> = this.store.pipe(select(fromAuth.getUser));
   booksList: BookList;
+  droppedData: any;
   
-  constructor(private booksService:BooksListService) { 
+  constructor(private booksService:BooksListService, private store: Store<fromAuth.State>,
+    private collectionService: CollectionService) { 
     this.booksService.searchBooks('Colombia');
   }
 
   ngOnInit() {
+    this.user$.subscribe(user => {
+      this.collectionList = this.collectionService.getList(user);
+    });
     this.booksService.booksList
         .subscribe(
           books => {        
@@ -26,7 +36,7 @@ export class BooksMainComponent implements OnInit {
     );
   }
 
-  onDrop(event): void {
+  dragEnd(event): void {
     console.log("dropData: "+event);
   }
   
